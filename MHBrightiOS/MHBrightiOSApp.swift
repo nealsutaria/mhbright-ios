8bf756d38c331cfc18f1e207176665cbbdf29b2f
neal-sutaria <97972270+neal-sutaria@users.noreply.github.com>
2026-07-18 06:21:19 -0500

    Initial Commit

---
+++ MHBrightiOS/MHBrightiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MHBrightiOSApp.swift
+//  MHBrightiOS
+//
+//  Created by Neal Sutaria on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MHBrightiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
